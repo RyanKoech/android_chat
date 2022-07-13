04dc69a26565dcbfaa71461064e3401ae3a65bd2
RyanKoech <sirryankoech@gmail.com>
2022-07-13 15:42:21 +0300

    Implemented firebase login

--- app/src/main/java/com/example/android_chat/LoginActivity.kt
+++ app/src/main/java/com/example/android_chat/LoginActivity.kt
@@ -1,11 +1,58 @@
 package com.example.android_chat
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var editEmail: EditText
+    private lateinit var editPassword: EditText
+    private lateinit var btnLogin: Button
+    private lateinit var btnSignup: Button
+    private lateinit var mAuth : FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        supportActionBar?.hide()
+
+        mAuth = FirebaseAuth.getInstance()
+        editEmail = findViewById(R.id.edit_email)
+        editPassword = findViewById(R.id.edit_password)
+        btnLogin = findViewById(R.id.btn_login)
+        btnSignup = findViewById(R.id.btn_signup)
+
+        btnSignup.setOnClickListener{
+            val intent = Intent(this, SignupActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnLogin.setOnClickListener {
+            val email = editEmail.text.toString()
+            val password = editPassword.text.toString()
+
+            login(email, password)
+        }
+    }
+
+    private fun login(email : String, password : String) {
+        mAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    val intent = Intent(this@LoginActivity, MainActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Toast.makeText(this@LoginActivity, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 }
