a2c9ecb2646c676bb849635899755e53fbc95c00
RyanKoech <sirryankoech@gmail.com>
2022-07-15 15:12:11 +0300

    Added click listener to user list item

--- app/src/main/java/com/example/android_chat/adapters/UserAdapter.kt
+++ app/src/main/java/com/example/android_chat/adapters/UserAdapter.kt
@@ -1,13 +1,16 @@
 package com.example.android_chat.adapters
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.android_chat.ChatActivity
 import com.example.android_chat.R
 import com.example.android_chat.models.User
+import com.google.firebase.auth.FirebaseAuth
 
 class UserAdapter(val context: Context, val userList : ArrayList<User>) : RecyclerView.Adapter<UserAdapter.UserViewHolder>() {
 
@@ -19,6 +22,13 @@ class UserAdapter(val context: Context, val userList : ArrayList<User>) : Recycl
     override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
         val currentUser = userList[position]
         holder.textName.text = currentUser.name
+        holder.itemView.setOnClickListener{
+            val intent = Intent(context, ChatActivity::class.java)
+            intent.putExtra("NAME", currentUser.name)
+            intent.putExtra("UID", currentUser.uid)
+
+            context.startActivity(intent)
+        }
     }
 
     override fun getItemCount(): Int {
